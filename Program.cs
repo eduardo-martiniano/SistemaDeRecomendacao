be0ede75bd99a804840572b11be38ce1a14ee703
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2020-12-06 14:53:32 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaDeRecomendacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de recomendação");
+        }
+    }
+}
