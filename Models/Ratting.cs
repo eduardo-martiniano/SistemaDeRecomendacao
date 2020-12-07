4936dc9da87ad5201bc8a8e22be03f185c19ad73
Diego <diego_alessandro@hotmail.com>
2020-12-07 14:30:07 -0300

    adiciona métodos para a busca de filmes

---
+++ Models/Ratting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecomendacaoConsole.Models
+{
+    public class Ratting
+    {
+        public int FilmeId { get; set; }
+        public float Nota { get; set; }
+    }
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecomendacaoConsole.Models
+{
+    public class Tag
+    {
+        public string Nome { get; set; }
+    }
+}
